fa33cf563dde80462f8db64dcf0aae84d95bba27
arif-odemis <arif.odemis@hotmail.com>
2022-12-31 17:13:03 +0300

    components update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthorComponent } from './components/author/author.component';
+import { CategoryComponent } from './components/category/category.component';
+import { NaviComponent } from './components/navi/navi.component';
+import { BookshelfComponent } from './components/bookshelf/bookshelf.component';
+import { PublisherComponent } from './components/publisher/publisher.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthorComponent,
+    CategoryComponent,
+    NaviComponent,
+    BookshelfComponent,
+    PublisherComponent
   ],
   imports: [
     BrowserModule,
